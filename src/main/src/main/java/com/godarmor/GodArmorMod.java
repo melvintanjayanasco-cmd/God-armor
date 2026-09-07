2318428f782910b83e2a45aecc280b186fcb0193
melvintanjayanasco-cmd <melvintanjayanasco@gmail.com>
2026-09-07 09:04:40 +0800

    Create GodArmorMod.java

---
+++ src/main/src/main/java/com/godarmor/GodArmorMod.java
@@ -0,0 +1,13 @@
+package com.godarmor;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GodArmorMod implements ModInitializer {
+
+    public static final String MOD_ID = "godarmor";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("God Netherite Armor mod has loaded!");
+    }
+  }
